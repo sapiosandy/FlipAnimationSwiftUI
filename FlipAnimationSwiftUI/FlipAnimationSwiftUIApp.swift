6a0698feb17e23b544f284787033057000c0ed89
Sandy Gomez <sgomez1029@gmail.com>
2024-05-27 00:21:33 -0700

    Initial Commit

---
+++ FlipAnimationSwiftUI/FlipAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipAnimationSwiftUIApp.swift
+//  FlipAnimationSwiftUI
+//
+//  Created by Sandra Gomez on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FlipAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
